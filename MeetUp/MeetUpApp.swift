391b11519fa2d6f800526b82892938404816f105
Faustharus <chailloleau-damien@outlook.fr>
2024-07-01 20:19:24 +0200

    Initial Commit

---
+++ MeetUp/MeetUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetUpApp.swift
+//  MeetUp
+//
+//  Created by Damien Chailloleau on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MeetUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
